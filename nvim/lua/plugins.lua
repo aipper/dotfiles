7984907f74ad0c2e5dd28237c1558a39ea0e7231
ab <aipper@qq.com>
2022-05-10 09:16:01 +0800

    fix

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,27 @@
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local paccker_bootstrap
+if fn.empty(fn.glob(install_path)) > 0 then
+  vim.notify("正在安装Pakcer.nvim，请稍后...")
+  paccker_bootstrap = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1", -- "https://github.com/wbthomason/packer.nvim",
+    "https://gitcode.net/mirrors/wbthomason/packer.nvim",
+    install_path,
+  })
+
+  -- https://github.com/wbthomason/packer.nvim/issues/750
+  local rtp_addition = vim.fn.stdpath("data") .. "/site/pack/*/start/*"
+  if not string.find(vim.o.runtimepath, rtp_addition) then
+    vim.o.runtimepath = rtp_addition .. "," .. vim.o.runtimepath
+  end
+  vim.notify("Pakcer.nvim 安装完毕")
+end
+
+vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'preservim/nerdtree'
+end)
