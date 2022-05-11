62a2a91b3cd85015f3436da3a241b49a78c1e2f2
ab <aipper@qq.com>
2022-05-11 16:38:22 +0800

    fix

--- nvim/lua/conf/onedark.lua
+++ nvim/lua/conf/onedark.lua
@@ -2,8 +2,8 @@
 local onedark = require("onedark")
 onedark.setup  {
     -- Main options --
-    style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    transparent = true,  -- Show/hide background
+    style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = false,  -- Show/hide background
     term_colors = true, -- Change terminal color as per the selected theme style
     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
